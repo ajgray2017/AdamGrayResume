52e91789a8a90bff02be2caf07f6c050412dd198
Adam Gray <ajgray2017@gmail.com>
2020-01-27 12:30:57 -0500

    added in 455 project and edited in snake

--- Snake/modules.js
+++ Snake/modules.js
@@ -15,9 +15,40 @@ var drawingModule = (function(){
         ctx.fillRect(x*snakeSize+1, y*snakeSize+1, snakeSize-2, snakeSize-2);
     }
 
+    //! move to bottom
     var scoreText = function() {
         var score_text = "Score: " + score;
         ctx.fillStyle = 'blue';
         ctx.fillText(score_text, 145, h-5);
     }
+    
+    var createFood = function() {
+        food = {
+            //random position
+          x: Math.floor((Math.random() * 30) + 1),
+          y: Math.floor((Math.random() * 30) + 1)
+        } 
+      
+      for (var i=0; i>snake.length; i++) {
+          var snakeX = snake[i].x;
+          var snakeY = snake[i].y;
+
+            //if snakes head was at the same postion as food 
+            if (food.x===snakeX || food.y === snakeY || food.y === snakeY && food.x===snakeX) {
+                food.x = Math.floor((Math.random() * 30) + 1);
+                food.y = Math.floor((Math.random() * 30) + 1);
+            }
+        }
+    }
+
+    var checkCol = function(x, y, array){
+        for(var i = 0; i < array.length; i++){
+            if(array[i].x === x && array[i].y === y)
+            return true;
+        }
+        return false;
+    }
+
+    
+
 }());
