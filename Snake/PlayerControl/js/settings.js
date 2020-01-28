69fdea00cc603c0b20d30155813bb53a02533022
Adam Gray <ajgray2017@gmail.com>
2020-01-28 10:41:51 -0500

    Updated names

--- Snake/PlayerControl/js/settings.js
+++
@@ -1,9 +0,0 @@
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-var snakeSize = 10;
-var w = 500;
-var h = 500;
-var score = 0;
-var snake;
-var snakeSize = 10;
-var food;
